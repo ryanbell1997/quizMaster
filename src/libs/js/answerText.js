252b05ad3d5a497225fe4576da91cbb025ce5d85
Ryan Bell <ryan.bell1997@hotmail.co.uk>
2020-11-15 22:49:08 +0000

    Updates working as expected

--- src/libs/js/answerText.js
+++ src/libs/js/answerText.js
@@ -3,14 +3,32 @@ import React from 'react';
 class AnswerText extends React.Component {
     constructor(props){
         super(props);
+        this.handleDeleteAnswer = this.handleDeleteAnswer.bind(this);
+        this.handleUpdateAnswer = this.handleUpdateAnswer.bind(this);
     }
 
+    handleDeleteAnswer() {
+        const { index, deleteAnswer } = this.props;
+        deleteAnswer(index);
+    }
+
+    handleUpdateAnswer(event) {
+        const answerText = event.target.value;
+        const { updateAnswer, index} = this.props;
+        updateAnswer(index, answerText);
+    }
+
+
+    
     render(){
+        const { index, answer } = this.props;
+        const placeholderText = "Enter answer here...";
         return (
             <div className="input">
-                <label htmlFor="answer1">Answer {this.props.qNo}</label>
-                <input name="answer1    " type="text" id={`answer_${this.props.qNo}`}></input>
+                <label htmlFor="answer">Answer {index+1}:</label>
+                <input name="answer" type="text" placeholder={placeholderText} onChange={this.handleUpdateAnswer} value={answer}></input>
                 <input type="radio" name="answerCheck" id="answerCheck" ></input>
+                <button type="button" onClick={this.handleDeleteAnswer}>X</button>
             </div>
         )
     }
--- src/libs/js/quizBuilder.js
+++ src/libs/js/quizBuilder.js
@@ -7,35 +7,94 @@ class QuizBuilder extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            noQuestions: 0
+            questions: []
         };
         this.addTextQuestion = this.addTextQuestion.bind(this);
+        this.addAnswer = this.addAnswer.bind(this);
+        this.deleteAnswer = this.deleteAnswer.bind(this);
+        this.updateAnswer = this.updateAnswer.bind(this);
+        this.removeQuestion = this.removeQuestion.bind(this);
+        this.updateQuestion = this.updateQuestion.bind(this);
     }
 
     addTextQuestion() {
-        this.setState({
-            noQuestions: this.state.noQuestions + 1
-        });
+        //let { questions } this.state == this.state.questions!!
+        let { questions } = this.state;
+        //text and answers keys are intiialised in newQuestion.
+        let newQuestion = { text: '', answers: [] }
+        questions.push(newQuestion);
+        //we set the state to the new list of questions.
+        this.setState({ questions });
+    }
+
+    removeQuestion(questionIndex){
+        let { questions } = this.state;
+        const questionsLen = questions.length;
+        if (questionsLen){
+            console.log(questionIndex);
+            questions.splice(questionIndex, 1);
+        this.setState({ questions });
+        }  
+    }
+
+    addAnswer(questionIndex, newAnswer) {
+        let { questions } = this.state;
+        //We get the specific questions object from state using the index (which is passed as prop), and then we add an answer to the answers array.
+        questions[questionIndex].answers.push(newAnswer);
+        this.setState({ questions });
+    }
+
+    deleteAnswer(questionIndex, answerIndex) {
+        let { questions } = this.state;
+        const questionsLen = questions.length;
+        if (questionsLen) {
+            questions[questionIndex].answers.splice(answerIndex, 1);
+        this.setState({ questions });
+        }
+    }
+
+    updateAnswer(questionIndex, answerIndex, answerText) {
+        let { questions } = this.state;
+        questions[questionIndex].answers[answerIndex] = answerText;
+        this.setState({ questions });
+    }
+
+    updateQuestion(questionIndex, questionText) {
+        let { questions } = this.state;
+        questions[questionIndex].text = questionText;
+        this.setState({ questions });
     }
 
     render(){
-        let children = [];
+        let questionChildren = [];
 
-        for(let i =  0; i < this.state.noQuestions; i++){
-            children.push(<AddTextQuestion key={i} qNo={i + 1}/>)
-        }
+        const { questions } = this.state;
 
-    return (
-        <div>
-            <h2 className="builderHeader">Quiz Builder</h2>
-            <div className="questionDiv" id="newQuestionContainer"></div>
-            <form className="addQForm">
-            {children}
-            </form>           
-            <button type="button" id="addQuestion" onClick={this.addTextQuestion}><i className="material-icons">add</i> Question</button>
-            
-        </div>
-    )
+        let i=0;
+        questions.forEach(question => {
+            questionChildren.push(<AddTextQuestion 
+                key={`q_${i}`}
+                questionIndex={i} 
+                question={question} 
+                addAnswer={this.addAnswer} 
+                deleteAnswer={this.deleteAnswer}
+                updateAnswer={this.updateAnswer}
+                removeQuestion={this.removeQuestion} 
+                updateQuestion={this.updateQuestion} />);
+            i++
+        })
+
+        return (
+            <div>
+                <h2 className="builderHeader">Quiz Builder</h2>
+                <div className="questionDiv" id="newQuestionContainer"></div>
+                <form className="addQForm">
+                {questionChildren}
+                </form>           
+                <button type="button" id="addQuestion" onClick={this.addTextQuestion}><i className="material-icons">add</i> Question</button>
+
+            </div>
+        )
    }  
 }
 
